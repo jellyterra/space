2b1c48c96c0696d68ee23579a4dff4838cf09d84
Jelly Tea <80201595+jellytea@users.noreply.github.com>
2024-02-01 12:16:29 +0000

    Fixed login options in space.py

--- space.py
+++ space.py
@@ -7,10 +7,10 @@ import sys
 home = os.getenv("HOME")
 
 if len(sys.argv) == 1:
-    subprocess.run(["podman", "exec", "-ti", "space", "su"])
+    subprocess.run(["podman", "exec", "-ti", "space", "su", "-Pl"])
 else:
     username = sys.argv[1]
     if len(sys.argv) == 2:
-        subprocess.run(["podman", "exec", "-ti", "space", "su", "-P", username, "-c", "bash"])
+        subprocess.run(["podman", "exec", "-ti", "space", "su", "-Pl", username, "-c", "bash"])
     else:
-        subprocess.run(["podman", "exec", "-ti", "space", "su", "-P", username, "-c"]+sys.argv[2:])
+        subprocess.run(["podman", "exec", "-ti", "space", "su", "-Pl", username, "-c"]+sys.argv[2:])
