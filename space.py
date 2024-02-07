3b089faf0b1dc322328157d53ec81ec7fd2e6a22
Jelly Tea <80201595+jellytea@users.noreply.github.com>
2024-02-07 14:35:12 +0000

    Replace su instead of sudo to handle options correctly.

--- space.py
+++ space.py
@@ -7,10 +7,7 @@ import sys
 home = os.getenv("HOME")
 
 if len(sys.argv) == 1:
-    subprocess.run(["podman", "exec", "-ti", "space", "su", "-Pl"])
+    subprocess.run(["podman", "exec", "-ti", "space", "sudo", "-i"])
 else:
     username = sys.argv[1]
-    if len(sys.argv) == 2:
-        subprocess.run(["podman", "exec", "-ti", "space", "su", "-Pl", username, "-c", "bash"])
-    else:
-        subprocess.run(["podman", "exec", "-ti", "space", "su", "-Pl", username, "-c"]+sys.argv[2:])
+    subprocess.run(["podman", "exec", "-ti", "space", "sudo", "-iu", username]+sys.argv[2:])
